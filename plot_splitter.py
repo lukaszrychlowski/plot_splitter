c2aceb525693bce06f390b6ba16f4e4d436d8f2a
lukaszrychlowski <rychlowski@agh.edu.pl>
2022-03-04 14:20:36 +0100

    multiple y axes

--- plot_splitter.py
+++ plot_splitter.py
@@ -20,34 +20,57 @@ def file_reader(path):
     return df
 
 ''' plot with mouse event implemented '''
-def plotter(df, x, y):
+def plotter_general(df, x, y, color):
     fig, ax = plt.subplots()
-    ax.plot(df[x],df[y], 'o', markersize=1, picker=3)
+    ax.plot(df[x],df[y], linewidth=1, c=color, picker=3)
     cid = fig.canvas.mpl_connect('pick_event', on_pick)
-    plt.tight_layout()
-    plt.grid(which='both', color='gray', linewidth=0.5, alpha=0.5)
-    plt.minorticks_on()
-    plt.ylim(bottom=-10, top=570)
-    #plt.xlim(left=0, right=20)
-    #plt.xlabel('Strain [%]')
-    #plt.ylabel('Stress [MPa]')
     return None
 
+def plotter_splitted(df,x,y1,y2,y3):
+    fig, ax = plt.subplots()
+    twin1 = ax.twinx()
+    twin2 = ax.twinx()
+    twin2.spines['right'].set_position(("axes", 1.2))
+    plot1 = ax.plot(x, y1, 'o', markersize=1, c='black')
+    plot2 = twin1.plot(x, y2, 'o', markersize=1, c='red')
+    plot3 = twin2.plot(x, y3, 'o', markersize=1, c='blue')
+    ax.set_xlabel(x)
+    ax.set_ylabel(y1)
+    twin1.set_ylabel(y2)
+    twin2.set_ylabel(y3)
+    ax.yaxis.label.set_color('black')
+    twin1.yaxis.label.set_color('red')
+    twin2.yaxis.label.set_color('blue')
+    #ax.legend()
 
  ### 34 clicksy
 df = file_reader(path)
-plotter(df,'time','F')
+plotter_general(df,'time','F', 'black')
 plt.show()
 
 for i in range(len(points)):
     try:
         df_filtered = df[(df['time'] >= points[i]) & (df['time'] <= points[i+1])]
         i += 1
-        plotter(df_filtered,'time','F')
-        #plt.plot(df_filtered['time'],df_filtered['F'], 'o', markersize=1, c='black')
-        #plt.savefig('/Users/ryszard/Downloads/wykresy/' + )
-        #plt.show()
+        fig1, ax1 = plt.subplots()
+        fig1.subplots_adjust(right=0.75)
+        twin1 = ax1.twinx()
+        twin2 = ax1.twinx()
+        twin2.spines['right'].set_position(('axes', 1.2))
+        ax1.plot(df_filtered['time'], df_filtered['F'], 'o', markersize=3, c='black', label='force')
+        twin1.plot(df_filtered['time'], df_filtered['T'], 's', markersize=3, c='red', label='temp')
+        twin2.plot(df_filtered['time'], df_filtered['v'], '*', markersize=3, c='blue', label='velocity')
+        ax1.set_xlabel('time')
+        ax1.set_ylabel("force")
+        twin1.set_ylabel("temp")
+        twin2.set_ylabel("velocity")
+        ax1.tick_params(axis='y', colors='black')
+        twin1.tick_params(axis='y', colors='red')
+        twin2.tick_params(axis='y', colors='blue')
+        plt.savefig('/Users/ryszard/Downloads/wykresy/' + str(i) + '.png', dpi=300)
+        plt.show()
+        plt.close()
     except IndexError:
         points.pop()
 
-plt.show()
+#plt.show()
